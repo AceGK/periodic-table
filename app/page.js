fc07d7e846432a6f213fef7d8d7099fb69283b27
Ace <acekisch@gmail.com>
2024-03-04 16:14:01 -0800

    styling update

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
 
   return (
     <main className="container">
-      <div className="pb-3">
+      <div className="pb-3" style={{textAlign:"center"}}>
         {/* <ThemeSwitch /> */}
         <h1>The Periodic Table of Elements</h1>
         <p>Select an element to view its details</p>

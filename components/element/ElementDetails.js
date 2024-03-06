81d02cc0538d940eaaf7f5a1c47ebf59cc6659e8
Ace <acekisch@gmail.com>
2024-03-06 10:25:21 -0800

    update styling

--- components/element/ElementDetails.js
+++ components/element/ElementDetails.js
@@ -12,6 +12,36 @@ const GLBViewerWithNoSSR = dynamic(() => import("../GLBViewer"), {
 export default function ElementDetails({ element, setSelectedElement }) {
   const [showDetails, setShowDetails] = useState(true);
 
+  return (
+    element && (
+      <div>
+        <div className={styles.toggleDetails}>
+          {/* <button onClick={()=>setShowDetails(!showDetails)}>
+            {showDetails ? <FaChevronUp /> : <FaChevronDown />}
+            </button> */}
+        </div>
+        {showDetails && <div className={styles.elementDetails}>
+          <Element element={element} />
+
+          <div className={styles.details}>
+
+
+            <Details element={element} />
+            <div style={{ maxHeight: "220px", maxWidth: "220px" }}>
+              {element.bohr_model_3d && (
+                <GLBViewerWithNoSSR path={element.bohr_model_3d} />
+              )}
+            </div>
+
+          </div>
+        </div>
+        }
+      </div>
+    )
+  );
+}
+
+function Details({ element }) {
   // State to keep track of the selected ionization energy index
   const [selectedIonizationIndex, setSelectedIonizationIndex] = useState(0);
 
@@ -23,159 +53,138 @@ export default function ElementDetails({ element, setSelectedElement }) {
   };
 
   return (
-    element && (
-      <div>
-        <div className={styles.toggleDetails}>
-          {/* <button onClick={()=>setShowDetails(!showDetails)}>
-            {showDetails ? <FaChevronUp /> : <FaChevronDown />}
-            </button> */}
-          </div>
-      {showDetails && <div className={styles.elementDetails}>
-        <Element element={element} />
-
-        <div className={styles.details}>
-          <div style={{ maxHeight: "220px", maxWidth:"220px" }}>
-            {element.bohr_model_3d && (
-              <GLBViewerWithNoSSR path={element.bohr_model_3d} />
-            )}
-          </div>
-
-          <div>
-            <ul data-type="element-data">
-              <li>
-                <label>Series</label>
-                <output>
-                  <span className="capitalize">{element.category}</span>
-                  {element.possible_category &&
-                    ` (${element.possible_category})`}
-                </output>
-              </li>
-
-              <li>
-                <label>Description</label>
-                <output>
-                  <Link href={element.source}>Wiki</Link>
-                </output>
-              </li>
-
-              {/* TODO make state at:temp selector */}
-              <li>
-                <label>State</label>
-                <output>{element.phase}</output>
-              </li>
-              <li>
-                <label>Abundance</label>
-                <output>...</output>
-              </li>
-              <li>
-                <label>Discovered</label>
-                <output>
-                  {element.discovered_by || 'n/a'}
-                  {element.discovered && `(${element.discovered})`}
-                </output>
-              </li>
-              <li>
-                <label>Named</label>
-                <output>
-                  {element.named_by || "n/a"} 
-                 {element.year_named && element.year_named}
-                </output>
-              </li>
-            </ul>
-          </div>
-
-          <div>
-            <ul data-type="element-data">
-              <li>
-                <label>Atomic Mass</label>
-                <output>{element.atomic_mass}u</output>
-              </li>
-              <li>
-                <label>Melting point</label>
-                <output>{element.melt} K</output>
-              </li>
-              <li>
-                <label>Boling point</label>
-                <output>{element.boil} K</output>
-              </li>
-              <li>
-                <label>Energy levels</label>
-                <output>
-                  {element.shells.map((i, index) => {
-                    // Check if the current item is the last in the array
-                    const isLastItem = index === element.shells.length - 1;
-                    // If it's the last item, don't append a comma. Otherwise, append a comma.
-                    return isLastItem ? `${i}` : `${i}, `;
-                  })}
-                </output>
-              </li>
-              <li>
-                <label>Electronegativity</label>
-                <output>{element.electronegativity_pauling}</output>
-              </li>
-              <li>
-                <label>Electron Affinity</label>
-                <output>{element.electron_affinity} kJ/mol</output>
-              </li>
-              <li>
-                <label style={{ display: "flex" }}>
-                  Ionization
-                  <select
-                    value={selectedIonizationIndex}
-                    onChange={(e) => setSelectedIonizationIndex(e.target.value)}
-                  >
-                    {element.ionization_energies.map((energy, index) => (
-                      <option key={index} value={index}>
-                        {getOrdinalLabel(index + 1)}
-                      </option>
-                    ))}
-                  </select>
-                </label>
-                <output>
-                  {element.ionization_energies[selectedIonizationIndex]} kJ/mol
-                </output>
-              </li>
-            </ul>
-          </div>
-
-          <div>
-            <ul data-type="element-data">
-             
-              <li>
-                <label>Radius</label>
-                <output>...</output>
-              </li>
-              <li>
-                <label>Hardness</label>
-                <output>...</output>
-              </li>
-              <li>
-                <label>Modulus</label>
-                <output>...</output>
-              </li>
-              <li>
-                <label>Density</label>
-                <output>{element.density}</output>
-              </li>
-              <li>
-                <label>Conductivity</label>
-                <output>...</output>
-              </li>
-              <li>
-                <label>Heat</label>
-                <output>...</output>
-              </li>
-            </ul>
-          </div>
-
-          {/* <div className={styles.summary}>
+    <>
+      {/* <div className={styles.summary}>
         <p>{element.summary}</p>
         <Link href={element.source}>Wiki</Link>
       </div> */}
-        </div>
+      <div>
+        <ul data-type="element-data">
+          <li>
+            <label>Series</label>
+            <output>
+              <span className="capitalize">{element.category}</span>
+              {element.possible_category &&
+                ` (${element.possible_category})`}
+            </output>
+          </li>
+
+          <li>
+            <label>Description</label>
+            <output>
+              <Link href={element.source}>Wiki</Link>
+            </output>
+          </li>
+
+          {/* TODO make state at:temp selector */}
+          <li>
+            <label>State</label>
+            <output>{element.phase}</output>
+          </li>
+          <li>
+            <label>Abundance</label>
+            <output>...</output>
+          </li>
+          <li>
+            <label>Discovered</label>
+            <output>
+              {element.discovered_by || 'n/a'}
+              {element.discovered && `(${element.discovered})`}
+            </output>
+          </li>
+          <li>
+            <label>Named</label>
+            <output>
+              {element.named_by || "n/a"}
+              {element.year_named && element.year_named}
+            </output>
+          </li>
+        </ul>
       </div>
-}
+
+      <div>
+        <ul data-type="element-data">
+          <li>
+            <label>Atomic Mass</label>
+            <output>{element.atomic_mass}u</output>
+          </li>
+          <li>
+            <label>Melting point</label>
+            <output>{element.melt} K</output>
+          </li>
+          <li>
+            <label>Boling point</label>
+            <output>{element.boil} K</output>
+          </li>
+          <li>
+            <label>Energy levels</label>
+            <output>
+              {element.shells.map((i, index) => {
+                // Check if the current item is the last in the array
+                const isLastItem = index === element.shells.length - 1;
+                // If it's the last item, don't append a comma. Otherwise, append a comma.
+                return isLastItem ? `${i}` : `${i}, `;
+              })}
+            </output>
+          </li>
+          <li>
+            <label>Electronegativity</label>
+            <output>{element.electronegativity_pauling}</output>
+          </li>
+          <li>
+            <label>Electron Affinity</label>
+            <output>{element.electron_affinity} kJ/mol</output>
+          </li>
+          <li>
+            <label style={{ display: "flex" }}>
+              Ionization
+              <select
+                value={selectedIonizationIndex}
+                onChange={(e) => setSelectedIonizationIndex(e.target.value)}
+              >
+                {element.ionization_energies.map((energy, index) => (
+                  <option key={index} value={index}>
+                    {getOrdinalLabel(index + 1)}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <output>
+              {element.ionization_energies[selectedIonizationIndex]} kJ/mol
+            </output>
+          </li>
+        </ul>
       </div>
-    )
-  );
+
+      <div>
+        <ul data-type="element-data">
+
+          <li>
+            <label>Radius</label>
+            <output>...</output>
+          </li>
+          <li>
+            <label>Hardness</label>
+            <output>...</output>
+          </li>
+          <li>
+            <label>Modulus</label>
+            <output>...</output>
+          </li>
+          <li>
+            <label>Density</label>
+            <output>{element.density}</output>
+          </li>
+          <li>
+            <label>Conductivity</label>
+            <output>...</output>
+          </li>
+          <li>
+            <label>Heat</label>
+            <output>...</output>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
 }

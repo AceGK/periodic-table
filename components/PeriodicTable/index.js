e3917a06369af1b753a2d8ff459dd9dbca3b4a9b
Ace <acekisch@gmail.com>
2024-03-06 09:55:49 -0800

    styling update

--- components/PeriodicTable/index.js
+++ components/PeriodicTable/index.js
@@ -26,20 +26,20 @@ export default function PeriodicTable({ }) {
   return (
     <>
 
-        <section className="container">
-      <ElementDetails element={selectedElement} setSelectedElement={setSelectedElement} />
+      <section className="container">
+        <ElementDetails element={selectedElement} setSelectedElement={setSelectedElement} />
       </section>
-        <div className="table-wrapper">
-          <div className={table ? "periodic-table" : "periodic-list"}>
+      <div className="table-wrapper">
+        <div className={table ? "periodic-table" : "periodic-list"}>
 
-            {/* <button className="toggleView" onClick={() => setTable(!table)}>
+          {/* <button className="toggleView" onClick={() => setTable(!table)}>
               {!table ? <PeriodicTableIcon /> : <FaThList />}
             </button> */}
 
-            {table &&
-            <section className="tabs" style={{padding:'1rem'}}>
+          {table &&
+            <section className="tabs" style={{ padding: '1rem' }}>
               <Legend setSelectedGroup={setSelectedGroup} setHoveredGroup={setHoveredGroup} />
-            {/* <Tabs>
+              {/* <Tabs>
               <Tab label="Filters">
               <Legend setSelectedGroup={setSelectedGroup} />
               </Tab>
@@ -50,21 +50,45 @@ export default function PeriodicTable({ }) {
               Coming soon...
               </Tab>
             </Tabs> */}
-            </section> }
+            </section>}
 
-            {/* <div className="searchForm">
+          {/* <div className="searchForm">
               <input type="text" placeholder="Search" />
             </div> */}
 
-            <div className="f-block-separator"></div>
-
-            {data.elements.map((element) => (
-              <Element key={element.number} element={element} handleElementClick={() => handleElementClick(element)} isGroupSelected={isGroupSelected} selectedElement={selectedElement} hoveredGroup={hoveredGroup} />
-            ))}
+          <div
+            className="lanthanide-card lanthanide"
+            onClick={() => setSelectedGroup(
+              selectedGroup.includes("lanthanide")
+                ? selectedGroup.filter(group => group !== "lanthanide") // Remove if it's already selected
+                : [...selectedGroup, "lanthanide"] // Add if it's not already selected
+            )}
+            onMouseEnter={() => setHoveredGroup("lanthanide")}
+            onMouseLeave={() => setHoveredGroup(null)}
+          >
+            57-71
           </div>
 
+          <div
+            className="actinide-card actinide"
+            onClick={() => setSelectedGroup(
+              selectedGroup.includes("actinide")
+                ? selectedGroup.filter(group => group !== "actinide") // Remove if it's already selected
+                : [...selectedGroup, "actinide"] // Add if it's not already selected
+            )}
+            onMouseEnter={() => setHoveredGroup("actinide")}
+            onMouseLeave={() => setHoveredGroup(null)}
+          >
+            89-103
+          </div>
+
+          {data.elements.map((element) => (
+            <Element key={element.number} element={element} handleElementClick={() => handleElementClick(element)} isGroupSelected={isGroupSelected} selectedElement={selectedElement} hoveredGroup={hoveredGroup} />
+          ))}
         </div>
-   
+
+      </div>
+
     </>
   );
 }
